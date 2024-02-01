68799cdd8b61c9459b6e0fc876cc43008547eda4
indra120 <indrapranata120@gmail.com>
2024-02-01 17:50:44 +0800

    Create a simple counter

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,16 @@
+import { useState } from "react"
+
 const App = () => {
-  return <div>Hello World</div>
+  const [count, setCount] = useState<number>(0)
+  
+  return (
+    <div>
+      <h1>{count}</h1>
+
+      <button onClick={() => setCount((prev) => prev + 1)}>Plus</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>Minus</button>
+    </div>
+  )
 }
 
 export default App
