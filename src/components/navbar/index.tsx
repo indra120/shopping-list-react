a38e18fbbde1fc1dcf70ef2f502458432247bb46
indra120 <indrapranata120@gmail.com>
2024-02-04 17:43:10 +0800

    Tailwind#2

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,10 @@
-import styles from "./navbar.module.css"
+// import styles from "./navbar.module.css"
 import shoppingIcon from "@/assets/shopping-icon.svg"
 
 const Navbar = () => (
-  <nav className={styles.nav}>
-    <img src={shoppingIcon} alt="shopping icon" className={styles.navIcon} />
-    <h1 className={styles.navTitle}>Shopping List</h1>
+  <nav className="flex justify-center items-center text-center bg-[#818cf8] py-0 px-4 h-14">
+    <img src={shoppingIcon} alt="shopping icon" className="size-6" />
+    <h1 className="text-[#f3f4f6] text-2xl font-bold">Shopping List</h1>
   </nav>
 )
 
